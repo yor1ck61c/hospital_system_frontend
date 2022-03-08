7372949060304ccf50cf5f88a823df34f6e22158
yorick_61c <1240371570@qq.com>
2022-03-08 13:06:22 +0800

    finish clear

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,30 +1,22 @@
 <template>
-  <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
-  </div>
+  <p>clear</p>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
-  computed: {
-    ...mapGetters([
-      'name'
-    ])
+  name: 'Homepage',
+  data() {
+    return {
+    }
+  },
+  created() {
+  },
+  methods: {
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
+
 </style>
